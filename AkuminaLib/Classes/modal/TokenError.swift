42f223f0e4392a468bd22ed3b838be69f039f81c
AnburajPandi <anburaj.pandi@akumina.com>
2023-03-25 16:52:20 +0530

    Files added

---
+++ AkuminaLib/Classes/modal/TokenError.swift
@@ -0,0 +1,13 @@
+//
+//  TokenError.swift
+//  AkuminaLib
+//
+//  Created by Mac on 25/03/23.
+//
+
+import Foundation
+
+enum TokenError: Error {
+    case tokenNotGeneratedError
+    case tokenNotFoundError
+}
